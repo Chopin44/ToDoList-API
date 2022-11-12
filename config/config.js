277e2e93d5246219f839dcbdf4118771cdd8c59b
Rodhi Andriansah <86567180+Chopin44@users.noreply.github.com>
2022-11-12 23:07:50 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = async() => {
     try {
-        await mongoose.connect('mongodb://mongo:********@containers-us-west-119.railway.app:6890');
+        await mongoose.connect('mongodb://mongo:yyRYRXusJr4Gb0jKmZ9v@containers-us-west-119.railway.app:6890');
         console.log("Database terkoneksi!");
       } catch (error) {
         console.log(error);
